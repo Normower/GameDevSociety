007a2d133d84acb3ce5801106397c646a0463aea
darker9999 <darker99999@googlemail.com>
2013-02-25 19:08:55 +0000

    Update Editor_Extension_Example/Script/LookAtPointScript.cs

--- Editor_Extension_Example/Script/LookAtPointScript.cs
+++ Editor_Extension_Example/Script/LookAtPointScript.cs
@@ -2,17 +2,24 @@ using UnityEngine;
 using System.Collections;
 using UnityEditor;
 
-[ExecuteInEditMode()]  
+/*
+        Created by Matt Norman for Game Dev Society
+  
+            The script we are creating a custom editor for. It simply has an onject face a specific position.
+*/
+
+[ExecuteInEditMode()]//script is active in the editor, allows us to see live changes.  
 public class LookAtPointScript : MonoBehaviour {
 
-   [SerializeField]
+   [SerializeField]//saves changes made
     private Vector3 lookAtPoint;
 
-    public Vector3 LookAtPoint
+    public Vector3 LookAtPoint//it's bad code practice to expose variables, by using this we can apply our own validation.
     {
         get { return lookAtPoint; }
         set
         {
+           //some form of validation if we needed it.
             lookAtPoint = value;
         }
     }
