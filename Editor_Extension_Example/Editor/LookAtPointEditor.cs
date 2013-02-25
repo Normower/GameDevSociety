a161e335cd878a1d5c0b26f82f85b38d9acb14d2
darker9999 <darker99999@googlemail.com>
2013-02-25 19:00:13 +0000

    Update Editor_Extension_Example/Editor/LookAtPointEditor.cs

--- Editor_Extension_Example/Editor/LookAtPointEditor.cs
+++ Editor_Extension_Example/Editor/LookAtPointEditor.cs
@@ -2,19 +2,22 @@ using UnityEngine;
 using System.Collections;
 using UnityEditor;
 
+/*
+        Created by Matt Norman for Game Dev Society
+  
+            This creates the editor for LookAtPointScript, place it in "Editor" folder inside of assets.
+*/
 
-[CustomEditor(typeof(LookAtPointScript))]
+[CustomEditor(typeof(LookAtPointScript))]//references the class we want to create an editor for.
 public class LookAtPointEditor : Editor
 {
-
     public override void OnInspectorGUI()
     {
-        LookAtPointScript myTarget = (LookAtPointScript)target;
-        myTarget.LookAtPoint = EditorGUILayout.Vector3Field("Look At Point", myTarget.LookAtPoint);
+        LookAtPointScript myTarget = (LookAtPointScript)target;//access point to the class's functions, variables etc.
+        myTarget.LookAtPoint = EditorGUILayout.Vector3Field("Look At Point", myTarget.LookAtPoint);//creates a vector3 field
         if (GUI.changed)
         {
-            EditorUtility.SetDirty(target);
-        }
-        
+            EditorUtility.SetDirty(target);//sets the changes
+        }        
     }
 }
